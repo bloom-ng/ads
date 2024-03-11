9d12dfe0c015aa9d29cbcaf73c98321ef3ca03d1
emmy <segun8427@gmail.com>
2024-03-11 15:38:39 +0100

    Added currency and discount

---
+++ database/migrations/2024_03_11_143359_add_columns_to_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string("currency")->default("₦");
+            $table->decimal("discount", 10, 2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            //
+        });
+    }
+}
