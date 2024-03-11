dc879bf48f5c884bbb3702d460b452215cde34ac
bloom-ng <it@bloomdigitmedia.com>
2024-03-11 12:50:27 +0000

    Changes from server

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $invoice = Invoice::latest()->paginate(20);
+        return response()->json($invoice, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'vat' => 'required|numeric',
+            'date' => 'required|date',
+            'service_charge' => 'required|numeric',
+            'billed_to_line_1' => 'required|string|max:255',
+            'billed_to_line_2' => 'required|string|max:255',
+            'billed_to_line_3' => 'required|string|max:255',
+            'account_name' => 'required|string|max:255',
+            'account_number' => 'required|numeric',
+            'bank_name' => 'required|string|max:255',
+            'line_items' => 'required|json',
+        ]);
+        $input = $request->all();
+        $invoice = Invoice::create($input);
+
+        return response()->json([
+            'message' => "Success",
+            'status' => 'success',
+            'data' => $invoice
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $invoice = Invoice::where('id', $id)->get();
+        if (!$invoice) {
+            return response()->json([
+                'message' => "Invoice not found",
+                'status' => 'error',
+            ], 404);
+        } else {
+            return response()->json([
+                'message' => "Success",
+                'status' => 'success',
+                'data' => $invoice
+            ], 200);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+$data = $request->validate([
+            'vat' => 'sometimes|numeric',
+            'date' => 'sometimes|date',
+            'service_charge' => 'sometimes|numeric',
+            'billed_to_line_1' => 'sometimes|string|max:255',
+            'billed_to_line_2' => 'sometimes|string|max:255',
+            'billed_to_line_3' => 'sometimes|string|max:255',
+            'account_name' => 'sometimes|string|max:255',
+            'account_number' => 'sometimes|string',
+            'bank_name' => 'sometimes|string|max:255',
+            'line_items' => 'sometimes|json',
+        ]);
+        $invoice = Invoice::find($id);
+        $invoice->update($data);
+
+        return response()->json([
+            'message' => "Success",
+            'status' => 'success',
+            'data' => $invoice
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Invoice::destroy($id);
+         return response()->json([
+            'message' => "Success",
+            'status' => 'success'
+        ], 204);
+    }
+}
---
+++ app/Models/Invoice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+protected $fillable = [
+        'vat',
+        'service_charge',
+        'date',
+        'billed_to_line_1',
+        'billed_to_line_2',
+        'billed_to_line_3',
+        'account_name',
+        'account_number',
+        'bank_name',
+        'line_items'
+    ];
+}
